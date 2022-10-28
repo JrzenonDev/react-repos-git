a4ffc26592830354cb1466c4b86d77a380ac7c20
JrzenonDev <jrobertoonb@gmail.com>
2022-10-28 16:09:22 -0300

    feat: get api repos and issuis, useEffect and useState

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,3 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import { Container } from "./styles";
+
 export default function Repositorio() {
-  return <h1>Repos</h1>;
+  const { repositorio } = useParams();
+
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load() {
+      const nameRepo = repositorio;
+
+      const [repoData, issuesData] = await Promise.all([
+        api.get(`/repos/${nameRepo}`),
+        api.get(`/repos/${nameRepo}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepo(repoData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repositorio]);
+
+  return <Container></Container>;
 }
